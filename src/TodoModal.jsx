fa3348d7ca087d6ef11e9c1e7585102caa014831
Adam Bethlehem <30418746+adambeth@users.noreply.github.com>
2024-08-10 11:49:12 +0200

    fixed item z-index

--- src/TodoModal.jsx
+++ src/TodoModal.jsx
@@ -7,7 +7,7 @@ function TodoModal({ onSetIsModal }) {
     onSetIsModal: PropTypes.func.isRequired,
   };
   return (
-    <div className=" z-1000 absolute top-0 left-0 w-full h-lvh flex justify-center items-center bg-slate-300 opacity-100">
+    <div className=" z-10 absolute top-0 left-0 w-full h-lvh flex justify-center items-center bg-slate-300 opacity-100">
       <div className="w-4/5 grid-cols-2 h-4/5">
         <CreateNewTodoForm onSetIsModal={onSetIsModal} />
       </div>
