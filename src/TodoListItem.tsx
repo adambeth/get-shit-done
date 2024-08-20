e76225e5ffa144196678a9ab4733be96eb08a9ca
Adam Bethlehem <30418746+adambeth@users.noreply.github.com>
2024-08-20 09:53:31 +0200

    Update TodoListItem.tsx
    Conditional Styling

--- src/TodoListItem.tsx
+++ src/TodoListItem.tsx
@@ -1,8 +1,9 @@
 import { FC } from 'react';
 import "./TodoListItem.css";
+import { Todo } from './App';
 
 export type Props={
-  todo:object,
+  todo:Todo,
   onClick: () => void,
   onDeleteTodo: (e:React.MouseEvent<HTMLButtonElement>) => void
   onHandleMarkAsDone:(e:React.MouseEvent<HTMLButtonElement>)=> void
@@ -13,7 +14,7 @@ const TodoListItem: FC<Props> = ({ todo, onClick, onDeleteTodo,onHandleMarkAsDon
   return (
     <div
       onClick={onClick}
-      className="border-solid border-4 border-orange-500 rounded-lg p-4 bg-slate-100 h-96"
+      className={`border-solid border-4 ${todo.isComplete ? `border-orange-500`:`border-gray-500 bg-slate-700` } rounded-lg p-4 bg-slate-100 h-96`}
     >
       <div>
         {" "}
@@ -39,8 +40,10 @@ const TodoListItem: FC<Props> = ({ todo, onClick, onDeleteTodo,onHandleMarkAsDon
           {description}
         </p>
       </div>
-      <button onClick={onHandleMarkAsDone} className="bg-orange-500 hover:bg-orange-700 text-white font-bold py-2 w-full px-4 rounded m-2">
-        Mark As Done
+      <button onClick={onHandleMarkAsDone} className="bg-orange-500 hover:bg-orange-700 text-white font-bold py-2 w-full px-4 rounded m-2">{
+        `${todo.isComplete ? "Mark As Done":"Re-Open" }`  
+        }
+        
       </button>
       <button
         onClick={onDeleteTodo}
