a82f2b78a71a89d5525e65296ac218cc975ba284
Adam Bethlehem <30418746+adambeth@users.noreply.github.com>
2024-08-22 07:50:02 +0200

    Create home page and routes

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,15 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import App from "./App.js";
 import "./index.css";
+import Routes from "../src/routes/Routes"
+
 
 const rootElement = document.getElementById("root");
 
 if (rootElement) {
   createRoot(rootElement).render(
     <StrictMode>
-      <App />
+      <Routes/>
     </StrictMode>
   );
 }
