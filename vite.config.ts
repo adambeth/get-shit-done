2afafc764280da5d62a65067ab6511a933f3b472
Adam Bethlehem <30418746+adambeth@users.noreply.github.com>
2024-08-22 10:24:54 +0200

    routes

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "./get-shit-done/",
+  base: "./",
   server: { watch: { usePolling: true } },
 });
